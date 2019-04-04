6d0c0d45786e5c788b35cf0df0da33047dab817d
schmax <schmax@gmx.de>
2019-04-04 09:30:50 +0200

    Update betterflat.py

--- betterflat.py
+++ betterflat.py
@@ -4,12 +4,14 @@ import sys
 
 def printInfo():
     print ("------------------------------------------------------------------------------")
-    print ("             X-PLANE betterflat   version 0.1 (test)  by schmax")
+    print ("             X-PLANE betterflat   version 0.12 (test)  by schmax")
     print ("This script adapts an X-Plane dsf-file to flatten height at an airport.")
-    print ("Usage: betterflat <dsf-filename> <apt.dat of airport> <opt: new heigth in m>")
+    print ("Usage: betterflat <dsf-filename> <apt.dat including airport>")
+    print ("               <opt: 4 letter uppercase ICAO Code> <opt: new heigth in m>")
     print ("Area to flatten is selected from airport boundary in apt.dat")
+    print ("If apt.dat includes several airports, select airport by ICAO-Code.")
     print ("Works for the moment only for single boundary without holes.")
-    print ("The new heigt is either given as optional third argument in meter or")
+    print ("The new heigt is either given as optional argument in meter or")
     print ("it is derived from average height of vertices at boundary.")
     print ("CAUTION: This is still a test-version. No warrenties/guaranties!")
     print ("         Existing .org files are overwritten!!!!!")
@@ -53,23 +55,31 @@ def PointInPoly(p, poly): #test wether a point p with [lat, lon] coordinates lie
             
             
 
-def readBoundaryFromAPT(filename):
+def readBoundaryFromAPT(filename, icao_id = ""):
 #
 # Reads coordinates from airport boundary in given apt.dat file and returns them in a list of [lon, lat] values
+# In case apt.dat contains severla airports the right airport is selected by giving the correct ICAO Code
 # Currently only works for single airports with single boundary defintion without holes!!!
 # For Bezier nodes of the boundary only the node without Bezier definitions is considered!!!
 #
+    if icao_id == "":
+        Airport = True #if no icao code is given the first boundary in file is selected
+    else:
+        Airport = False #else first the correct icoa id has to be found befor Airport becomes true
     BoundarySection = False  #is set true when we are in boundary section in .apt file
     l = [] #list of boundary coordinates that will be returned
     if not os.path.isfile(filename):
         print("Error: Airport File", filename, "does not exist!")
         sys.exit(1)
-    with open(filename, "r") as f:
+    with open(filename, encoding="utf8") as f:
         for line in f:
             v = line.split()
             if len(v) == 0: #don't consider empty lines
                 continue
-            if v[0] == '130':
+            if len(v) > 2:
+                if v[0] == '1302' and v[1] == 'icao_code' and v[2] == icao_id:
+                    Airport = True
+            if Airport == True and v[0] == '130':
                 BoundarySection = True
             elif BoundarySection:
                 if v[0] == '111' or v[0] == '112':
@@ -77,17 +87,27 @@ def readBoundaryFromAPT(filename):
                 elif v[0] == '113' or v[0] == '114':
                     l.append([float(v[2]), float(v[1])]) #Note: Bezier definitins are not considered, just the base point
                     BoundarySection = False
+                    Airport = False #stop after read first boundary!!
     if l != []:
         l.append(l[0]) #form closed loop by adding again first vertex
     return l
                     
 
 ### MAIN ###
+newheight = None  #new height that should be applied not defined yet
+icao_id = ""      #icao code in case of searching apt.dat including several airports
+
 try:
     dsffile = sys.argv[1]
     aptfile = sys.argv[2]
-    if len(sys.argv) > 3:
-        newheight = int(sys.argv[3])
+    if len(sys.argv) == 4:
+        if sys.argv[3].isdigit():
+            newheight = int(sys.argv[3])
+        else:
+            icao_id = sys.argv[3]
+    elif len(sys.argv) == 5:
+        icao_id = sys.argv[3]
+        newheight = int(sys.argv[4])
 except:
     printInfo()
     sys.exit()
@@ -95,18 +115,20 @@ except:
 dsffile = os.fspath(dsffile)
 aptfile = os.fspath(aptfile)
 
-#dsffile = os.fspath(r'D:\Programmierung\DSF_Reader\+50-126.dsf')
-#aptfile = os.fspath(r'D:\Programmierung\DSF_Reader\apt.dat')
 
-print ("Reading", aptfile, "to extract airport boundary.")
-poly = readBoundaryFromAPT(aptfile) ## Require r befor string to not get ASCII bell with \a !!!!!!!!!!!!!!
-print ("Extracted boundary with", len(poly), "vertices.")
+print ("Reading", aptfile, "to extract boundary of airport", icao_id, flush = True)
+poly = readBoundaryFromAPT(aptfile, icao_id) 
+if len(poly):
+    print ("Extracted boundary with", len(poly), "vertices.", flush = True)
+else:
+    print ("Error: No boundary found in ", aptfile)
+    sys.exit(2)
 
 dsf = XPLNEDSF()
 if dsf.read(dsffile): #returns value > 0 in case of errors
-    sys.exit(1)
+    sys.exit(3)
 
-print("Reading of dsf-file completed. Checking now for mesh triangles intersecting airport boundary.......")
+print("Reading of dsf-file completed. Checking now for mesh triangles intersecting airport boundary.......", flush = True)
 miny, maxy, minx, maxx = dsf.BoundingRectangle(poly)
 s = set([])
 for p in dsf.PatchesInArea(miny, maxy, minx, maxx): 
@@ -119,7 +141,7 @@ for p in dsf.PatchesInArea(miny, maxy, minx, maxx):
                     s.add((t[0][0], t[0][1])) #add all 3 vertices of tria to set as tuples
                     s.add((t[1][0], t[1][1]))
                     s.add((t[2][0], t[2][1]))
-        if PointInPoly(t[0], poly): #Check also that not complete Tria lies in poly by checking for first vertex of Tria
+        if PointInPoly(TriaV[0], poly): #Check also that not complete Tria lies in poly by checking for first vertex of Tria
             s.add((t[0][0], t[0][1])) #add all 3 vertices of tria to set as tuples
             s.add((t[1][0], t[1][1]))
             s.add((t[2][0], t[2][1]))        
@@ -137,7 +159,7 @@ for v in s:
 averageheight = round(sum / count)
 print ("Average height of boundary vertexes:", averageheight)
 
-if len(sys.argv) < 4:
+if newheight == None:
     newheight = averageheight
     print("Take this average height", newheight,"(in m) as new height for according vertices.")
 else:
